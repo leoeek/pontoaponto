9156c74a56fd9b77c3a7eff6fcf9dced67ab6511
leoprocopio <leonardolp@gmail.com>
2024-10-11 08:14:00 -0300

    add analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,13 @@ export default defineNuxtConfig({
   // https://github.com/nuxt-themes/alpine
   extends: '@nuxt-themes/alpine',
 
-  modules: [
-    // https://github.com/nuxt-modules/plausible
-    '@nuxtjs/plausible',
-    // https://github.com/nuxt/devtools
-    '@nuxt/devtools'
-  ],
+  modules: [// https://github.com/nuxt-modules/plausible
+  '@nuxtjs/plausible', // https://github.com/nuxt/devtools
+  '@nuxt/devtools', 'nuxt-gtag'],
 
-  compatibilityDate: '2024-09-28'
+  compatibilityDate: '2024-09-28',
+
+  gtag: {
+    id: 'G-GM2J20XEEL'
+  }
 })
